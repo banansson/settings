a18555c5690575784b3589b5464150492a2439ca
Jan Olofsson <banansson@gmail.com>
2011-07-22 14:50:15 +0200

    reminder about creating junction to dropbox folder for the vimwiki plugin

--- vim/install.bat
+++ vim/install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem mklink requires admin rights or the SetCreateSymbolicLinkPrivilege
+rem mklink requires admin rights or the SeCreateSymbolicLinkPrivilege
 rem which can be changed in the securiy policy.
 
 setlocal
@@ -8,3 +8,11 @@ set src=%~dp0
 set dst=%HOMEDRIVE%%HOMEPATH%\
 mklink /J "%dst%vimfiles" "%src%"
 mklink "%dst%_vimrc" "%src%vimrc"
+
+echo.
+echo Some plugins requires a path to the dropbox folder
+echo so don't forget to create the junction:
+echo  1. cd C:\Users\[user]
+echo  2. mklink /J Dropbox [dropbox location]
+echo.
+endlocal
