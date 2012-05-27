d9574664790d1d0c5ac676762f577d60b7b76783
Jan Olofsson <banansson@gmail.com>
2012-05-27 19:34:39 +0200

    creating directory for terminalrc if not yet created

--- xfce/install.sh
+++ xfce/install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+TERMINAL_DIR=~/.config/Terminal
 TERMINALRC_SOURCE=$PWD/terminal/terminalrc
-TERMINALRC_TARGET=~/.config/Terminal/terminalrc
+TERMINALRC_TARGET=$TERMINAL_DIR/terminalrc
+
+if [ ! -d $TERMINAL_DIR ]; then
+  echo "No directory for Terminal config found, creating..."
+  mkdir -p $TERMINAL_DIR
+fi
 
 if [ -e $TERMINALRC_TARGET ]; then
-  echo "found existing terminalrc, making a backup..."
+  echo "Found existing terminalrc, making a backup..."
   mv $TERMINALRC_TARGET $TERMINALRC_TARGET-backup
 fi
 
 cp $TERMINALRC_SOURCE $TERMINALRC_TARGET
 echo "$TERMINALRC_SOURCE copied to $TERMINALRC_TARGET"
-echo "for some reason linking it won't make terminal pick ut the changes"
+echo "For some reason linking it won't make terminal pick ut the changes"
  
