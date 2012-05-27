df309cfa1eb27316481b55d56401cd973723535a
Jan Olofsson <banansson@gmail.com>
2012-05-27 21:26:40 +0200

    script to add keepass2 ubuntu repository

---
+++ ubuntu/repos/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Adding repository for keepass2"
+sudo apt-add-repository ppa:jtaylor/keepass
+echo "Now run:"
+echo "  sudo apt-get update"
+echo "  sudo apt-get install keepass2"
+
